286edbac155e092b58d856d36927fdd9e820ca1e
Mel Huang <hello@melaniehuang.com>
2017-03-27 15:34:29 +1100

    playing with textures

--- js/minerals.js
+++ js/minerals.js
@@ -1,17 +1,22 @@
+function preload(){
+  t = loadImage("images/pour-grey.gif");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw(){
-  background(0);
-  translate(0,-200,0);
-  for(var i = 0; i < 20; i++){
+  background("#cecece");
+
+  translate(0,-100,0);
+  texture(t);
+  for (var i = 0; i < 15; i++){
     noStroke();
     translate(i,i,12);
     rotateY(frameCount*0.005);
     rotateX(frameCount*0.01);
-    cylinder(12+i, 200);
+    cylinder(10+i, 200);
   }
 
 }
