cee333b1db8270ed3d3601f1ce1618b06cd1951e
Mel Huang <hello@melaniehuang.com>
2017-03-27 14:49:00 +1100

    long boxes crystal

--- js/minerals.js
+++ js/minerals.js
@@ -1,15 +1,15 @@
 function setup() {
-  createCanvas(600, 600, WEBGL);
+  createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw(){
   background(0);
-
+  translate(0,-200,0);
   for(var i = 0; i < 10; i++){
     noStroke();
     translate(i,i,10);
-    rotateY(frameCount*0.01);
-    rotateX(frameCount*0.02);
-    box(20,20,100);
+    rotateY(frameCount*0.005);
+    rotateX(frameCount*0.01);
+    box(20,20,120)
   }
 }
